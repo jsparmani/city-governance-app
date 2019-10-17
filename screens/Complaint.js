cec78a02beb0842f322840e245def42a406db169
parthsujalshah <parthsujalshah@gmail.com>
2019-10-17 17:18:56 +0530

    First

---
+++ screens/Complaint.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class Complaint extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Complaint </Text>
+            </View>
+        )
+    }
+}
