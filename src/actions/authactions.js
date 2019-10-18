6612dcde4cd2259a1b091a59c5fb161a595d4b5b
Jay Parmani <jsparmani@gmail.com>
2019-10-19 01:46:14 +0530

    resolve error

--- src/actions/authactions.js
+++ src/actions/authactions.js
@@ -6,12 +6,12 @@ import {
     LOGIN_CHECK
 } from "./types";
 import axios from "axios";
-import { AsyncStorage } from "react-native";
+import {AsyncStorage} from "react-native";
 
 axios.defaults.baseURL = "http://192.168.137.1:8000/api/";
 
-export const loginUser = ({ username, password }, props) => async dispatch => {
-    dispatch({ type: LOGIN_USER });
+export const loginUser = ({username, password}, props) => async dispatch => {
+    dispatch({type: LOGIN_USER});
 
     axios
         .post("user/token/", {
@@ -30,63 +30,61 @@ export const loginUser = ({ username, password }, props) => async dispatch => {
 const loginUserSuccess = async (dispatch, token, props) => {
     await AsyncStorage.setItem("auth_token", `Token ${token}`);
 
-    axios.post('user/check-user/', {
-        token: token
-    }).then(res => {
-        type = res.data.type;
+    axios
+        .post("user/check-user/", {
+            token: token
+        })
+        .then(res => {
+            type = res.data.type;
 
-        if (res.data.department) {
-            dept_id = res.data.department_id;
-        }
+            if (res.data.department) {
+                dept_id = res.data.department_id;
+            }
 
-        dispatch({
-            type: LOGIN_USER_SUCCESS,
-            payload: [token, type, dept_id]
+            dispatch({
+                type: LOGIN_USER_SUCCESS,
+                payload: [`Token ${token}`, type, dept_id]
+            });
+
+            props.navigation.navigate("Home");
         });
-
-        props.navigation.navigate("Home");
-    })
-
-
 };
 
 const loginUserFail = dispatch => {
-    dispatch({ type: LOGIN_USER_FAIL });
+    dispatch({type: LOGIN_USER_FAIL});
 };
 
 export const logoutUser = navigation => async dispatch => {
     await AsyncStorage.removeItem("auth_token");
-    dispatch({ type: LOGOUT_USER });
+    dispatch({type: LOGOUT_USER});
     navigation.navigate("Login");
 };
 
 export const loginCheck = navigation => async dispatch => {
     token = await AsyncStorage.getItem("auth_token");
     if (token) {
+        const raw_token = token.split(" ")[1];
+        axios
+            .post("user/check-user/", {
+                token: raw_token
+            })
+            .then(res => {
+                if (res.status === 200) {
+                    type = res.data.type;
 
-        const raw_token = token.split(" ")[1]
-        axios.post('user/check-user/', {
-            token: raw_token
-        }).then(res => {
-            if (res.status === 200) {
-                type = res.data.type;
+                    if (res.data.department) {
+                        dept_id = res.data.department_id;
+                    }
+                    dispatch({
+                        type: LOGIN_CHECK,
+                        payload: [`${token}`, type, dept_id]
+                    });
 
-                if (res.data.department) {
-                    dept_id = res.data.department_id;
+                    navigation.navigate("Home");
+                } else {
+                    alert("Not 200");
                 }
-                dispatch({
-                    type: LOGIN_CHECK,
-                    payload: [`${token}`, type, dept_id]
-                });
-
-                navigation.navigate("Home");
-            }
-            else {
-                alert('Not 200')
-            }
-        }).catch(err => {
-        })
-
-
+            })
+            .catch(err => {});
     }
 };
